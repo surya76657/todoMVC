528f9eb5308a926a6dac130c124e433d38631cbd
Himanshu kumar <himanshu4.kumar@paytm.com>
2019-11-03 00:33:59 +0530

    Updates

---
+++ precache-manifest.d69338e1fb839233354d5dce35f80020.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb9ac068c652f8a953982f766febde6",
+    "url": "/todoMVC/index.html"
+  },
+  {
+    "revision": "edc85c20de4229ac9ad8",
+    "url": "/todoMVC/static/css/main.37ab4bee.chunk.css"
+  },
+  {
+    "revision": "e9efe602f1675fa99d2d",
+    "url": "/todoMVC/static/js/2.c108e152.chunk.js"
+  },
+  {
+    "revision": "edc85c20de4229ac9ad8",
+    "url": "/todoMVC/static/js/main.e11e4a13.chunk.js"
+  },
+  {
+    "revision": "b8ea23322aef491413da",
+    "url": "/todoMVC/static/js/runtime-main.3312df2e.js"
+  }
+]);
